61066967ac4f866e4c46f03b819a8de126dc8a3a
felix <felix.deltenre@etu.univ-amu.fr>
2025-06-17 13:13:30 +0200

    ok

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers | Acme Dashboard',
+  description: 'View and manage your customer database',
+};
+
 export default function Page() {
     return <p>Customers Page</p>;
   }
